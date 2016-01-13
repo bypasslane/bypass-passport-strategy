bacecf679daf791afb0b8b536736ee9c2bc0c77a
Jonathan Spies <jonathan.spies@gmail.com>
2016-01-13 11:59:26 -0500

    full test support

--- spec/bypass_strategy_spec.js
+++ spec/bypass_strategy_spec.js
@@ -1,13 +1,99 @@
 /* global describe, it, expect */
+var mockery = require("mockery");
+var fs = require('fs');
 
-var Strategy = require('../lib/bypass_strategy');
+mockery.registerMock('./bypass_auth.js', {
+  login: function(server, key) {
+    if (key == "fail") {
+      return Promise.reject({statusCode: 401});
+    } else if (key == "error") {
+      return Promise.reject({statusCode: 500});
+    } else {
+      var response = JSON.parse(fs.readFileSync(__dirname + '/fixtures/auth.json'));
+      return Promise.resolve(response);
+    }
+  }
+});
 
-describe('Strategy', function() {
+mockery.enable({
+  warnOnReplace: false,
+  warnOnUnregistered: false,
+  useCleanCache: true
+});
 
-  var strategy = new Strategy(function(){});
+var BypassStrategy = require('../lib/bypass_strategy');
 
-  it('should be named bypasstoken', function() {
-    expect(strategy.name).toEqual('bypasstoken');
+describe('BypassStrategy', function() {
+
+  it('requires a server', function() {
+    expect(function() {
+      new BypassStrategy()
+    }).toThrow();
   });
 
+  describe('with proper params', function() {
+    var strategy;
+
+    beforeAll(function() {
+      strategy = new BypassStrategy({server: 'http://authme.com'});
+      strategy.fail = function() {}
+      strategy.success = function() {}
+      strategy.error = function() {}
+    });
+
+    afterAll(function() {
+      mockery.disable();
+    });
+
+    it('should be named bypasstoken', function() {
+      expect(strategy.name).toEqual('bypasstoken');
+    });
+
+    describe('authenticating', function() {
+      var request;
+
+      beforeEach(function() {
+        request = {
+          headers: {
+            'x-session-token': "valid"
+          }
+        }
+      });
+
+      it('calls fail if no token is present', function(done) {
+        spyOn(strategy, 'fail').and.callFake(function(msg) {
+          expect(strategy.fail).toHaveBeenCalledWith("No token found");
+          done();
+        });
+        strategy.authenticate({headers: {}});
+      });
+
+      it('calls fail() with Unauthorized if it receives a 401', function(done) {
+        spyOn(strategy, 'fail').and.callFake(function(msg) {
+          expect(strategy.fail).toHaveBeenCalledWith("Unauthorized");
+          done();
+        });
+        request.headers['x-session-token'] = 'fail';
+        strategy.authenticate(request, {});
+      });
+
+      it('calls success() with the user if it receives a 200', function(done) {
+        spyOn(strategy, 'success').and.callFake(function() {
+          expect(strategy.success).toHaveBeenCalled();
+          done();
+        });
+        strategy.authenticate(request);
+      });
+
+      it('calls error() with the error if it receives a 500', function(done) {
+        spyOn(strategy, 'error').and.callFake(function() {
+          expect(strategy.error).toHaveBeenCalled();
+          done();
+        });
+        request.headers['x-session-token'] = 'error';
+        strategy.authenticate(request);
+      });
+
+    });
+  });
 });
